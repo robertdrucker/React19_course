cc310baecb0b2ca39a45a9412d737496ac6d5bd4
Robert Drucker <robert.drucker@holycitysoftware.com>
2025-07-03 15:31:20 -0400

    From the ground up

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const App = () => (
+  <div>
+    <h1>Hello world !!!</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
