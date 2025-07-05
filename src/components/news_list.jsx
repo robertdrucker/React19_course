1e0e906e88ddca887617c4c86270dbdb567e4e43
Robert Drucker <robert.drucker@holycitysoftware.com>
2025-07-05 17:45:07 -0400

    Props

---
+++ src/components/news_list.jsx
@@ -0,0 +1,10 @@
+const NewsList = (props) => {
+  console.log(props);
+  return (
+    <>
+      <div>News list:</div>
+    </>
+  );
+};
+
+export default NewsList;
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import { createRoot } from "react-dom/client";
 import Header from "./components/header/header";
-// import StatePlayground from "./components/playground/state";
+import NewsList from "./components/news_list";
+import { newsData } from "./utils/data";
 import "./styles/styles.css";
 
-const App = () => (
-  <>
-    <Header />
-    <div className="container">{/* <StatePlayground /> */}</div>
-  </>
-);
+const App = () => {
+  let [news, setNews] = useState(newsData);
+
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <NewsList news={news} />
+      </div>
+    </>
+  );
+};
 
 createRoot(document.getElementById("root")).render(<App />);
