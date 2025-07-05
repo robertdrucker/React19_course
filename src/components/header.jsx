5be3a4543b671f978ff66942b9511b03c4ed9d66
Robert Drucker <robert.drucker@holycitysoftware.com>
2025-07-05 13:46:10 -0400

    Adding Styles - inline

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,18 @@
+let styles = {
+  header: {
+    backgroundColor: "#03a9fa",
+  },
+  logo: {
+    color: "fff",
+    fontFamily: "Antonio, sans-serif",
+    textAlign: "center",
+  },
+};
+
 const Header = () => {
   return (
-    <header>
-      <div>LOGO</div>
+    <header style={styles.header}>
+      <div style={styles.logo}>LOGO</div>
       <input />
     </header>
   );
