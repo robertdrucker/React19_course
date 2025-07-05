d7cd5ac4b1438327428e77b1628b518679931533
Robert Drucker <robert.drucker@holycitysoftware.com>
2025-07-05 17:03:02 -0400

    Prev State

--- src/components/playground/state.jsx
+++ src/components/playground/state.jsx
@@ -1,16 +1,32 @@
 import { useState } from "react";
 
 const StatePlayground = () => {
-  const [count, setCount] = useState(0);
+  const initialCount = 0;
+  const [count, setCount] = useState(initialCount);
 
-  // const addOne = () => {
-  //     setCount(count+1)
-  // }
+  const addOne = () => {
+    setCount((prevCount) => {
+      return prevCount + 1;
+    });
+  };
+
+  const subtOne = () => {
+    setCount((prevCount) => {
+      /// large calculation
+      return prevCount - 1;
+    });
+  };
+
+  const resetCount = () => {
+    setCount(initialCount);
+  };
 
   return (
     <>
       <h3>Count:{count}</h3>
-      <button onClick={() => setCount(count + 1)}>Add one +1</button>
+      <button onClick={addOne}>Add one +1</button>
+      <button onClick={subtOne}>Subt one -1</button>
+      <button onClick={resetCount}>RESET</button>
     </>
   );
 };
