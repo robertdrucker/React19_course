844418e2be40793cc1d7de4470468e4b00df780c
Robert Drucker <robert.drucker@holycitysoftware.com>
2025-07-05 19:00:11 -0400

    Inline stylings

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,14 +2,17 @@ import Navigation from "./nav";
 import { useState } from "react";
 
 const Header = () => {
+  const [active, setActive] = useState("active");
   const [keywords, setKeywords] = useState("");
 
   const handleChange = (event) => {
+    const value = event.target.value === "" ? "active" : "not-active";
     setKeywords(event.target.value);
+    setActive(value);
   };
 
   return (
-    <header>
+    <header className={active}>
       <div className="logo">Awesome News</div>
       <input onChange={handleChange} />
       The kewords are: {keywords}
