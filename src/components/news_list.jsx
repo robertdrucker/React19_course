6f88d157ab97c22bc6df380f991905fcfe9fe99f
Robert Drucker <robert.drucker@holycitysoftware.com>
2025-07-05 17:50:27 -0400

    Props to news item

--- src/components/news_list.jsx
+++ src/components/news_list.jsx
@@ -1,10 +1,9 @@
+import NewsItem from "./news_list_item";
+
 const NewsList = (props) => {
-  console.log(props);
-  return (
-    <>
-      <div>News list:</div>
-    </>
-  );
+  const news = props.news.map((item) => <NewsItem item={item} key={item.id} />);
+
+  return <>{news}</>;
 };
 
 export default NewsList;
---
+++ src/components/news_list_item.jsx
@@ -0,0 +1,8 @@
+const NewsItem = ({ item }) => (
+  <div className="news_item">
+    <h3>{item.title}</h3>
+    <div>{item.feed}</div>
+  </div>
+);
+
+export default NewsItem;
